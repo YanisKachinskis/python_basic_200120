46587c384dc9c84646850244d29eddab52e0a320
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-01-21 20:50:12 +0300

    Первый коммит

--- lesson1/hw2.py
+++
@@ -1,6 +0,0 @@
-seconds = int(input('Введите время в секундах: '))
-minutes = seconds // 60
-seconds_left = seconds % 60
-hours = minutes // 60
-minutes_left = minutes % 60
-print(f'{hours}:{minutes_left}:{seconds_left}')
--- lesson1/hw3.py
+++
@@ -1,3 +0,0 @@
-n = input('Чему равно число "n": ')
-result = int(n) + int(n + n) + int(n + n + n)
-print(result)
