264c6d66a2a0a91c9cf7ec21d71647e0ad1da623
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-01-26 16:02:30 +0300

    Внес изменения в hw2.py

--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -3,4 +3,4 @@ minutes = seconds // 60
 seconds_left = seconds % 60
 hours = minutes // 60
 minutes_left = minutes % 60
-print(f'{hours}:{minutes_left}:{seconds_left}')
+print(f'{hours:02}:{minutes_left:02}:{seconds_left:02}')
